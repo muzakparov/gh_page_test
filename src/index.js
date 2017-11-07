14234b11bccd606b0e5ba0f6766e2ddb34ee38ef
muzakparov <muzakparov@gmail.com>
2017-11-07 17:29:01 +0200

    add basename

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
-
+console.log("basename here")
 ReactDOM.render(
   <BrowserRouter basename="/gh_page_test" >
     <App />
